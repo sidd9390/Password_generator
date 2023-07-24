26a7106dd7edaa67e35e860c856dcfc1f55aafa1
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2023-07-24 23:39:49 +0530

    Files uploaded
    Files uploded

---
+++ script.js
@@ -0,0 +1,23 @@
+const passwordBox = document.getElementById("Password");
+const length = 14;
+const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase ="abcdefghijklmnopqrstuvwxyz";
+const number ="0123456789";
+const symbol ="@#$%!&*><}{()^=+\|-_[]~";
+const allChar =upperCase + lowerCase + number + symbol;
+function creatPassword(){
+    let password = "";
+    password +=upperCase[Math.floor(Math.random() * upperCase.length)];
+    password +=lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password +=number[Math.floor(Math.random() * number.length)];
+    password +=symbol[Math.floor(Math.random() * symbol.length)];
+    while(length > password.length){
+        password +=allChar[Math.floor(Math.random() * allChar.length)];
+    }
+    passwordBox.value = password;
+
+}
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
